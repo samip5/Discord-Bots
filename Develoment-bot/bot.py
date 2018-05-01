fc51c8ecc6d70d4ecb69e71b99d72248758b894c
samip5 <sm@samip.fi>
2018-05-01 16:18:49 +0300

    Updates, but Develoment bot is highly WIP.

--- Develoment-bot/bot.py
+++ Develoment-bot/bot.py
@@ -1,8 +1,8 @@
 ########################################################
-## Discord bot which integrates with Ombi and Plex.   ##
+##      Develoment Bot, may or may not work           ##
 ##                                                    ##
-## Author: samip5 				                      ##
-## Version: 0.2 				                      ##
+##                Author: samip5 				      ##
+##                                                    ##
 ########################################################
 
 import configparser as parser
@@ -23,27 +23,36 @@ def get_prefix(bot, message):
   prefixes = ['!']
   return commands.when_mentioned_or(*prefixes)(bot, message)
 
+
 initial_extensions = ['cogs.general']
 
-bot = commands.Bot(command_prefix=get_prefix, description="Samip's Advanced Bot")
+bot = commands.Bot(command_prefix=get_prefix, description="Samip's Develoment Bot")
+
 if __name__ == '__main__':
-        for extension in initial_extensions:
-                try:
-                        bot.load_extension(extension)
-                except:
-                        print(f'Failed to load extension {extension}.', file=sys.stderr)
-                        traceback.print_exc()
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
 @bot.event
 async def on_ready():
-        print (f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nAPI Version: {discord.__version__}\n')
-        await bot.change_presence(game=Game(name="with code"))
-        print (f'Successfully connected.')
+    print (f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nAPI Version: {discord.__version__}\n')
+    await bot.change_presence(game=Game(name="with code"))
+    print (f'Successfully connected.')
 
+@bot.event
+async def on_message(message):
+    isAdmin = message.author.permissions_in(message.channel).administrator
+    if not isAdmin:
+        await bot.say("Sorry, but you do not have sufficient privileges to access this bot.")
+    else:
+        await bot.process_commands(message)
+        
 @bot.event
 async def on_error(event, *args, **kwargs):
 	message = args[0]
 	await bot.say(message.channel, "You caused an error.")
-
 
 
 bot.run(token, bot=True, reconnect=True)
