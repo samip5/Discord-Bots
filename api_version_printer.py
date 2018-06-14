ee7857280cd10da8285b75c7f97dcfac0f22290f
Sami <samip5@users.noreply.github.com>
2018-06-14 23:55:56 +0300

    Updated main.py
    The comment block.

--- api_version_printer.py
+++
@@ -1,7 +0,0 @@
-import discord
-
-print(f'API Version: {discord.__version__}')
-if(discord.__version__ == "1.0.0a"):
-    print(f'We are using rewrite branch.')
-else:
-    print(f'We are not using rewrite branch.')
