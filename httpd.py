c8644e123e81e02e13ff19ad9a2a6c4e6f576fa1
Sami <samip5@users.noreply.github.com>
2018-06-21 11:16:33 +0300

    HTTPD testing #2

--- httpd.py
+++ httpd.py
@@ -1,16 +1,31 @@
 import os
+import http.mimetype
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+class samip537FunHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
 	def do_GET(self):
-		return
+		rootdir = os.getcwd()
+		try:
+			if self.path.endswitch('.html'):
+				f = open(rootdir + self.path)
+
+				self.send_response(200)
+				self.send_header('Contet-type', 'text/html')
+				self.end_headers()
+
+				self.wfile.write(f.read())
+				f.close()
+				return
+		except IOError:
+			self.send_error(404, 'file not found')
 
 	def do_HEAD(self):
-		return
-
-	def do_POST(self):
+		self.send_response(200)
+		self.send_header('Contet-type', 'text/html')
+		self.end_headers()
 		return
 
 
@@ -19,7 +34,7 @@ def run_httpd():
 
 	port = int(os.environ.get('PORT', 5000))
 	server_address = ('0.0.0.0', port)
-	httpd = HTTPServer(server_address,testHTTPServer_RequestHandler)
+	httpd = HTTPServer(server_address, samip537FunHTTPServer_RequestHandler)
 	httpd.serve_forever()
 
 
