bcea08200f463fda5436e6d086930e4e817c20b0
Sami <samip5@users.noreply.github.com>
2018-06-21 10:59:06 +0300

    More httpd updates

--- httpd.py
+++ httpd.py
@@ -5,9 +5,12 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
-		self.end_headers()
+		return
+
+	def do_HEAD(self):
+		return
+
+	def do_POST(self):
 		return
 
 
