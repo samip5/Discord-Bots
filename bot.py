d4000d90f2df26145cb159fb94bf51f05adf289a
Sami <samip5@users.noreply.github.com>
2018-06-21 08:40:59 +0300

    More cogs.

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@
 # Author: samip5
 
 
-import configparser as parser
 import sys
 import traceback
 
@@ -10,20 +9,13 @@ import discord
 from discord import Game
 from discord.ext import commands
 
-config = parser.ConfigParser()
-
-config.read('./config.ini')
-
-dev_token = config['secrets']['BOT-DEVELOMENT-TOKEN']
-token = config['secrets']['BOT-TOKEN']
-
 
 def get_prefix(bot, message):
 	prefixes = ['!!']
 	return commands.when_mentioned_or(*prefixes)(bot, message)
 
 
-initial_extensions = ['cogs.private', 'cogs.misc']
+initial_extensions = ['cogs.misc']
 
 bot = commands.Bot(command_prefix=get_prefix)
 
@@ -41,17 +33,6 @@ async def on_ready():
 	print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nAPI Version: {discord.__version__}\n')
 	await bot.change_presence(game=Game(name="with code"))
 	print(f'Successfully connected.')
-
-
-@bot.event
-async def on_message(message):
-	await bot.process_commands(message)
-
-
-@bot.event
-async def on_error(event, *args, **kwargs):
-	message = args[0]
-	await bot.say(message.channel, "You caused an error.")
 
 
 # bot.run(token, bot=True, reconnect=True)
